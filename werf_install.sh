955e71b24b37762193627a92a25242bad5b69d60
Sparks <zaharus000@mail.ru>
2020-01-26 12:36:42 +0700

    Update werf_install.sh

--- werf_install.sh
+++ werf_install.sh
@@ -1,4 +1,10 @@
-sudo apt install docker
+sudo apt update && sudo apt upgrade
+sudo apt install linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update && apt-cache policy docker-ce
+sudo apt install -y docker-ce
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
